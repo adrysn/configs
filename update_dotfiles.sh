dbbd40c392618594634beccab2a6f2dd477aadbb
Jonghyun Park <adrysn@gmail.com>
2021-07-04 22:39:28 +0900

    Add update dotfiles script

---
+++ update_dotfiles.sh
@@ -0,0 +1,6 @@
+\cp -f ./dotfiles/.my_aliases.sh ${HOME}/
+\cp -f ./dotfiles/.my_envs.sh ${HOME}/
+\cp -f ./dotfiles/.tmux.conf ${HOME}/
+\cp -f ./dotfiles/.vimrc ${HOME}/
+\cp -f ./dotfiles/.zpreztorc ${HOME}/
+\cp -f ./dotfiles/.zshrc ${HOME}/
