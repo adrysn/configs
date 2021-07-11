bf651627426a2a072bdd134ae0548f2da0dc3d47
Jonghyun Park <adrysn@gmail.com>
2021-07-11 13:23:18 +0900

    Evaluate two different pyenv init commands
    On some systems, the new init command is not enough to activate pyenv
    versions.

--- dotfiles/.my_envs.sh
+++ dotfiles/.my_envs.sh
@@ -22,6 +22,7 @@ fi
 if [ -d "$HOME/.pyenv" ]; then
     export PYENV_ROOT="$HOME/.pyenv"
     export PATH="$PYENV_ROOT/bin:$PATH"
+    eval "$(pyenv init -)"
     eval "$(pyenv init --path)"
 fi
 if [ -d "$HOME/.pyenv/plugins/pyenv-virtualenv" ]; then
